52b915c8b4ccf7cbd26227093662524e6b1fc607
braintrustbroker <37354336+braintrustbroker@users.noreply.github.com>
2023-05-19 04:22:07 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-if exist sleep.exe erase sleep.exe
+rem if exist sleep.exe erase sleep.exe
 ..\..\TCC\Tinycc\Win32\tcc.exe sleep.c -o sleep.exe -lwinmm -lgdi32 -lkernel32 -lmsvcrt -luser32 -ladvapi32
 
 PAUSE
